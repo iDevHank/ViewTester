ffd5dcdd4e2978d0e3c2a3d2f15c79b425611fac
Hank Zhang <hankzhang@pacewear.cn>
2017-07-03 15:45:11 +0800

    Initial Commit

---
+++ ViewTester/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ViewTester
+//
+//  Created by Hank on 03/07/2017.
+//  Copyright © 2017 Hank. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
